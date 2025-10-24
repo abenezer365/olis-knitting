b5265e371ba36ca7002f904dd69c827d33d1814b
abenezer365 <abenezerzewge0@gmail.com>
2025-10-24 11:04:15 +0300

    Merged Frontend Website #1.1.0

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,11 @@
-import React from "react";
-import Home from "./pages/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Story from "./pages/Story";
-import Products from "./pages/Products";
-import ProductsDetail from "./pages/ProductsDetail";
-import ContactUs from "./pages/ContactUs";
-import { CartProvider } from "./contexts/CartContext";
-import Dashboard from './Pages/Dashboard/Dashboard'
-
+import React from 'react'
+import Router from './Router'
 function App() {
   return (
-    <CartProvider>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/story" element={<Story />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/:id" element={<ProductsDetail />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path='/dashboard/*' element={<Dashboard />}/>   
-        </Routes>
-        <Footer />
-      </Router>
-    </CartProvider>
-  );
+    <>
+      <Router />
+    </>
+  )
 }
 
-export default App;
+export default App
